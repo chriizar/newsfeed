2b19b57cb261596e0269d01f897e709849907043
chriizar <ayon.chris@gmail.com>
2014-07-10 16:26:27 -0500

    Modificaciones - Publicar/Leer
    Se crearon los archivos para leer y publicar noticias.

---
+++ sdAPIClient/newsfeed/server.php
@@ -0,0 +1,83 @@
+<?php
+$noticias = array(
+				  array(
+				  	'titulo'      => "no seas putocvbcvbcvb",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  ),array(
+			 	  	'titulo'      => "yyyyyyyyyyyyyyyyyyyyyyyyyy",
+			 	  	'descripcion' => "descrdfgdfgdfgipcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  ),array(
+			 	  	'titulo'      => "pinche moy",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	  ,array(
+			 	  	'titulo'      => "titulo",
+			 	  	'descripcion' => "descripcion",
+			 	  	'fecha'       =>"12-julio"
+			 	  )
+			 	);
+$json = json_encode($noticias, true);
+echo $json;
